f804a2d78eb3a23a6b6010ee50446e6ba7c869f7
gregory Le Terte <gregory.leterte@gmail.com>
2021-03-01 23:57:06 +0100

    update key env secret

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(usersRoutes);
+//app.use(usersRoutes);
 
 app.use(
    jwt({
