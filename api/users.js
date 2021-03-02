632d36430faa2d028973919da1dd32620c5668e0
gregory Le Terte <gregory.leterte@gmail.com>
2021-03-02 22:12:56 +0100

    a

--- api/users.js
+++ api/users.js
@@ -7,7 +7,8 @@ const SHA256 = require("crypto-js/sha256");
 const encBase64 = require("crypto-js/enc-base64");
 
 // Import model User and Children
-const { Users } = require("../models/users");
+const User = require("../models/users");
+const Children = require("../models/Children");
 
 const isAuthenticated = (req, res, next) => {
    if (req.headers.authorization === `Bearer ${process.env.ACCESS_TOKEN}`) {
@@ -17,13 +18,17 @@ const isAuthenticated = (req, res, next) => {
    }
 };
 
-// Route signup
-router.post("/users/signup", isAuthenticated, async (req, res) => {
-   console.log(req.fields.email);
+// Route List User
+router.get("/api/users", isAuthenticated, async (req, res) => {
+   const user = await User.find();
+   res.status(200).json(user);
+});
 
+// Route signup
+router.post("/api/signup", isAuthenticated, async (req, res) => {
    try {
       // Search in the BDD.  Does a user have this email address ?
-      const user = await Users.findOne({ email: req.fields.email });
+      const user = await User.findOne({ email: req.fields.email });
 
       // If ok, return a message and do not proceed with registration
       if (user) {
@@ -43,11 +48,12 @@ router.post("/users/signup", isAuthenticated, async (req, res) => {
                salt: salt,
                account: {
                   userName: req.fields.userName,
+                  guidance: req.fields.guidance,
                   languageDefault: req.fields.languageDefault,
-                  secretCode: req.fields.secretcode,
-                  isActif: true,
-                  tutorialSeen: req.fields.tutorialSeen,
+                  secretCode: req.fields.secretCode,
                },
+               children: [],
+               isActif: true,
                createdAt: new Date(),
             });
 
@@ -70,9 +76,62 @@ router.post("/users/signup", isAuthenticated, async (req, res) => {
    }
 });
 
-router.post("/users/signin", isAuthenticated, async (req, res) => {
+// Route update
+router.post("/api/update", isAuthenticated, async (req, res) => {
    try {
-      const user = await Users.findOne({ email: req.fields.email });
+      // Search in the BDD.  Does a user have this Id ?
+      console.log(req.fields);
+      const user = await User.findById(req.fields._id);
+
+      // If ok, return a message and do not proceed with registration
+      if (!user) {
+         res.status(409).json({ message: "This user does not exist" });
+      } else {
+         // required information ?
+         if (
+            req.fields.email ||
+            req.fields.userName ||
+            req.fields.guidance ||
+            req.fields.languageDefault ||
+            req.fields.secretCode
+         ) {
+            req.fields.email && (user.email = req.fields.email);
+            req.fields.userName &&
+               (user.account.userName = req.fields.userName);
+            req.fields.guidance &&
+               (user.account.guidance = req.fields.guidance);
+            req.fields.languageDefault &&
+               (user.account.languageDefault = req.fields.languageDefault);
+            req.fields.secretCode &&
+               (user.account.secretCode = req.fields.secretCode);
+
+            if (req.fields.password) {
+               const salt = uid2(64);
+               const hash = SHA256(req.fields.password + salt).toString(
+                  encBase64
+               );
+               user.salt = salt;
+               user.hash = hash;
+            }
+            // Save in the bdd :
+            await user.save();
+            res.status(200).json({
+               _id: user._id,
+               email: user.email,
+               token: user.token,
+               account: user.account,
+            });
+         } else {
+            // user not sent the required information? ?
+            res.status(400).json({ message: "Missing parameters" });
+         }
+      }
+   } catch (error) {}
+});
+
+router.post("/api/signin", isAuthenticated, async (req, res) => {
+   try {
+      const user = await User.findOne({ email: req.fields.email });
 
       if (user) {
          if (
