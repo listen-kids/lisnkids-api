15f9b1851fd7c8a03a4a3a56e7032ec6065b297b
Maxence Derepas <maxence.derepas@gmail.com>
2021-03-05 12:21:53 +0100

    change routeAvatars

--- api/avatars.js
+++ api/avatars.js
@@ -5,82 +5,76 @@ const router = express.Router();
 const { avatars } = require("../models");
 
 const isAuthenticated = (req, res, next) => {
-   if (req.headers.authorization === `Bearer ${process.env.ACCESS_TOKEN}`) {
-      next();
-   } else {
-      res.json({ message: "Unauthorized" });
-   }
+    if (req.headers.authorization === `Bearer ${process.env.ACCESS_TOKEN}`) {
+        next();
+    } else {
+        res.json({ message: "Unauthorized" });
+    }
 };
 
-router.get("/api/avatars_all", isAuthenticated, async (req, res) => {
-   const avatar = await avatars.find();
-   res.status(200).json(avatar);
-});
-
 router.post("/api/avatars", async (req, res) => {
-   try {
-      if (req.body.title || req.body.selected || req.body.dayNightOnly) {
-         console.log(req.body);
-         const paramsReceive = {};
+    try {
+        if (req.body.title || req.body.selected || req.body.dayNightOnly) {
+            console.log(req.body);
+            const paramsReceive = {};
 
-         req.body.title && (paramsReceive.title = req.body.title.toUpperCase());
-         req.body.selected &&
-            (paramsReceive.selected = req.body.selected.toUpperCase());
-         req.body.dayNightOnly &&
-            (paramsReceive.dayNightOnly = req.body.dayNightOnly.toUpperCase());
+            req.body.title &&
+                (paramsReceive.title = req.body.title.toUpperCase());
+            req.body.selected &&
+                (paramsReceive.selected = req.body.selected.toUpperCase());
+            req.body.dayNightOnly &&
+                (paramsReceive.dayNightOnly = req.body.dayNightOnly.toUpperCase());
 
-         console.log(paramsReceive);
+            const response = await avatars.find(paramsReceive);
 
-         const response = await avatars.find(paramsReceive);
-
-         if (response.length > 0) {
-            res.status(200).json(response);
-         } else {
-            res.status(400).json({ message: "Not picture" });
-         }
-      } else {
-         // user not sent the required information? ?
-         res.status(400).json({ message: "Missing parameters" });
-      }
-   } catch (error) {
-      console.log(error.message);
-      res.status(400).json(error.message);
-   }
+            if (response.length > 0) {
+                res.status(200).json(response);
+            } else {
+                res.status(400).json({ message: "Not picture" });
+            }
+        } else {
+            const avatarAll = await avatars.find();
+            res.status(200).json(avatarAll);
+        }
+    } catch (error) {
+        console.log(error.message);
+        res.status(400).json(error.message);
+    }
 });
 
 //////////////////////////////////////
 // reserve admin request for import avatars///------------------------------------------
 //////////////////////////////////////
-router.post("/api/add_avatars", isAuthenticated, async (req, res) => {
-   try {
-      const testFolder = "/Users/maxencederepas/Downloads/AVATARS";
+router.post("/api/addAvatars", isAuthenticated, async (req, res) => {
+    try {
+        const testFolder = "/Users/maxencederepas/Downloads/AVATARS";
 
-      const files = await readdir(testFolder);
-      for await (const file of files) {
-         const words = file.split("_");
-         let pictureToUpload = testFolder + "/" + file;
-         const result = await cloudinary.uploader.upload(pictureToUpload, {
-            folder: "/lisnkids-avatars",
-         });
+        const files = await readdir(testFolder);
+        for await (const file of files) {
+            const words = file.split("_");
+            let pictureToUpload = testFolder + "/" + file;
+            const result = await cloudinary.uploader.upload(pictureToUpload, {
+                folder: "/lisnkids-avatars",
+            });
 
-         let chaine1 = words[3].split(".");
-         console.log(chaine1[0]);
+            let chaine1 = words[3].split(".");
+            console.log(chaine1[0]);
 
-         const newAvatar = new avatars({
-            title: words[1],
-            selected: chaine1[0],
-            dayNightOnly: words[2],
-            extension: ".png",
-            url: result.secure_url,
-         });
-         console.log(newAvatar);
-         await newAvatar.save();
-      }
-      res.status(200).json({ message: "avatar save" });
-   } catch (error) {
-      console.log(error.message);
-      res.status(400).json({ message: error.message });
-   }
+            const newAvatar = new avatars({
+                title: words[1],
+                selected: chaine1[0],
+                dayNightOnly: words[2],
+                extension: ".png",
+                url: result.secure_url,
+            });
+            console.log(newAvatar);
+            await newAvatar.save();
+        }
+        res.status(200).json({ message: "avatar save" });
+    } catch (error) {
+        console.log(error.message);
+        res.status(400).json({ message: error.message });
+    }
 });
 
 module.exports = router;
