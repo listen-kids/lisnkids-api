2eef6298013a2e57bd16cab3a69c1a28f4f86b48
Maxence Derepas <maxence.derepas@gmail.com>
2021-03-08 17:22:13 +0100

    update model avatar

--- models/childrens.js
+++ models/childrens.js
@@ -1,28 +1,40 @@
 module.exports = (mongoose, Mongoose) => {
-   const schema = Mongoose.Schema(
-      {
-         firstName: String,
-         nameAvatar: String,
-         age: Number,
-         isActif: {
-            type: Boolean,
-            default: true,
-         },
-         createdAt: Date,
-         updatedAt: { type: Date, default: Date.now },
+    const schema = Mongoose.Schema(
+        {
+            firstName: String,
+            avatar: {
+                nameAvatar: String,
+                daySelected: String,
+                dayUnselected: String,
+                nightSelected: String,
+                nightUnselected: String,
+                onlySelected: String,
+                onlyUnselected: String,
+            },
+            daySelected: String,
+            age: Number,
+            isActif: {
+                type: Boolean,
+                default: true,
+            },
+            createdAt: Date,
+            updatedAt: { type: Date, default: Date.now },
 
-         //historic_listens: {
-         //   type: [Mongoose.Schema.Types.ObjectId],
-         //   ref: "historic_listens",
-         //},
+            //historic_listens: {
+            //   type: [Mongoose.Schema.Types.ObjectId],
+            //   ref: "historic_listens",
+            //},
 
-         historic_listens: [
-            { type: Mongoose.Schema.Types.ObjectId, ref: "historic_listens" },
-         ],
-      },
-      {
-         timestamps: false,
-      }
-   );
-   return mongoose.model("childrens", schema, "childrens");
+            historic_listens: [
+                {
+                    type: Mongoose.Schema.Types.ObjectId,
+                    ref: "historic_listens",
+                },
+            ],
+        },
+        {
+            timestamps: false,
+        }
+    );
+    return mongoose.model("childrens", schema, "childrens");
 };
